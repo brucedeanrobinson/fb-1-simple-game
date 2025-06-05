b927668dc2ecaf36bf3d26d76a30d5a38f3f29a1
brucedeanrobinson <brucedeanrobinson>
2025-06-05 08:38:03 -0400

    extract logic to engine ofc

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-import { InitialGameState } from './game/game'
-import type { Board, EndState } from './game/game'
+import { initialGameState, move, } from './game/game'
 
 function App() {
-  const [game, setGame] = useState(InitialGameState())
+  const [game, setGame] = useState(initialGameState())
 
-  const move = (row: number, col: number) => {
-
-    // if clicked square is occupied, return
-    if (game.board[row][col] || game.endState) return
-    
-    // set the game with a deconstructed game, newBoard, nextPlayer
-    setGame((prevGame) => {
-      // structuredClone the board
-      const newGame = structuredClone(prevGame)
-      // set the new board[row][col] to the current player (x | o)
-      newGame.board[row][col] = prevGame.currentPlayer
-
-      // set next player as the other with a ternary
-      newGame.currentPlayer = prevGame.currentPlayer === 'x' ? 'o' : 'x'
-
-      // check win condition
-      newGame.endState = getEndState(newGame.board)
-
-      return newGame
-    })
-  }
-
-  const getEndState = (board: Board): EndState => {
-    // check wins
-    // row
-    for (let i = 0; i < 3; i++) {
-      const [a, b, c] = board[i]
-      if (a && a === b && a === c) return a
-    }
-
-    //col
-    for (let j = 0; j < 3; j++) {
-      const a = board[0][j]
-      const b = board[1][j]
-      const c = board[2][j]
-      if (a && a === b && a === c) return a
-    }
-
-    //diagonals
-    const center = board[1][1]
-    if (center && center === board[0][0] && center === board[2][2]) return center
-    if (center && center === board[0][2] && center === board[2][0]) return center
-
-    // check tie (board is full, so there isn't a win which would've been handled above)
-    // loop thru every row, checking every cell for non-null
-    if (board.every(row => row.every(cell => cell !== null))) return 'tie'
-
-    // else return null
-    return null;
+  const clickCell = (row: number, col: number) => {
+    setGame(prev => move(prev, row, col))
   }
 
   return (
@@ -66,7 +18,8 @@ function App() {
               <div
                 key={`${rowIndex}-${colIndex}`}
                 className="aspect-square border-2 border-white flex items-center justify-center text-5xl text-white cursor-pointer"
-                onClick={() => move(rowIndex, colIndex)}
+                //todo coords
+                onClick={() => clickCell(rowIndex, colIndex)}
               >
                 {cell}
               </div>
--- src/game/game.tsx
+++ src/game/game.tsx
@@ -1,11 +1,8 @@
 
 type Player = 'x' | 'o'
-
 type Cell = Player | null
-
 export type Board = Cell[][]
-
-export type EndState = 'x' | 'o' | 'tie' | null
+export type EndState = Player | 'tie' | null
 
 export type Game = {
   board: Board,
@@ -13,10 +10,57 @@ export type Game = {
   endState: EndState
 }
 
-export function InitialGameState(): Game {
+export function initialGameState(): Game {
   return {
     board: Array.from({length: 3}, () => Array(3).fill(null)),
     currentPlayer: 'x',
     endState: null
   }
+}
+
+// todo type for coords, function for nextplayer, 
+export const move = (game: Game, row: number, col: number) => {
+
+  // if clicked square is occupied, return
+  // doesn't return a game, but if we care on the client side to give feedback of a bad move then we will implement
+  if (game.board[row][col] || game.endState) return game
+
+  // structuredClone the board
+  const nextGame = structuredClone(game)
+  nextGame.board[row][col] = game.currentPlayer
+  
+  const nextPlayer: Player = game.currentPlayer === 'x' ? 'o' : 'x'
+
+  return { ...nextGame, currentPlayer: nextPlayer, endState: getEndState(nextGame) }
+}
+
+export const getEndState = (game: Game): EndState => {
+  const { board } = game;
+
+  // check wins
+  // row
+  for (let i = 0; i < 3; i++) {
+    const [a, b, c] = board[i]
+    if (a && a === b && a === c) return a
+  }
+
+  //col
+  for (let j = 0; j < 3; j++) {
+    const a = board[0][j]
+    const b = board[1][j]
+    const c = board[2][j]
+    if (a && a === b && a === c) return a
+  }
+
+  //diagonals
+  const center = board[1][1]
+  if (center && center === board[0][0] && center === board[2][2]) return center
+  if (center && center === board[0][2] && center === board[2][0]) return center
+
+  // check tie (board is full, so there isn't a win which would've been handled above)
+  // loop thru every row, checking every cell for non-null
+  if (board.every(row => row.every(cell => cell !== null))) return 'tie'
+
+  // else return null
+  return null;
 }
