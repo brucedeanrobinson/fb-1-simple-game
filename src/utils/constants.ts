38e396ff4504e7c50d937748ad4ed8c5628a584c
brucedeanrobinson <brucedeanrobinson>
2025-06-08 19:06:04 -0400

    main

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,4 @@
 export const PORT = 3000
-export const SERVER_URL = `http://localhost:${PORT}`
+// export const SERVER_URL = `http://localhost:${PORT}`
+export const SERVER_URL = `https://fb-tictactoe-production.up.railway.app`
+
